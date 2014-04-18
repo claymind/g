ada0a0246a51b6bfa9ab62d1c0da6facb57621b2
Guillermo Misa <support@claymind.com>
2014-04-18 02:26:06 -0700

    twitter test

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,16 +5,4 @@ angular.module('gigsetupApp', [
   'ngResource',
   'ngSanitize',
   'ngRoute'
-])
-  .config(function ($routeProvider, $locationProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'partials/main',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-      
-    $locationProvider.html5Mode(true);
-  });
+]);
